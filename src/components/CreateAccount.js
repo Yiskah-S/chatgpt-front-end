eecd67d4e07221a0f5d005db393def508ecd3318
Jessica <Yiskah.Scientia@gmail.com>
2023-07-30 17:24:09 -0400

    Create Account creates a new user to the db

---
+++ src/components/CreateAccount.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const CreateAccount = () => {
+	const [newUsername, setNewUsername] = useState('');
+	const [newEmail, setNewEmail] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		// Check if passwords match
+		if (newPassword !== confirmPassword) {
+			console.error('Passwords do not match');
+			return;
+		}
+
+		// Make a POST request to create a new user account
+		axios
+			.post(`${process.env.REACT_APP_BACKEND_URL}/users`, {
+				username: newUsername,
+				email: newEmail,
+				password: newPassword,
+			})
+			.then((response) => {
+				console.log('User created:', response.data);
+			})
+			.catch((error) => {
+				console.error('Error creating user:', error);
+			});
+	};
+
+	return (
+		<div className="container">
+			<h1>ChatGPT Crawler Site</h1>
+			<div id="create-account" className="visible">
+				<h2>Create Account</h2>
+				<form id="create-account-form" onSubmit={handleSubmit}>
+					<label htmlFor="new-username">Username:</label>
+					<input
+						type="text"
+						id="new-username"
+						name="new-username"
+						value={newUsername}
+						onChange={(e) => setNewUsername(e.target.value)}
+						required
+					/>
+
+					<label htmlFor="new-email">Email:</label>
+					<input
+						type="email"
+						id="new-email"
+						name="new-email"
+						value={newEmail}
+						onChange={(e) => setNewEmail(e.target.value)}
+						required
+					/>
+
+					<label htmlFor="new-password">Password:</label>
+					<input
+						type="password"
+						id="new-password"
+						name="new-password"
+						value={newPassword}
+						onChange={(e) => setNewPassword(e.target.value)}
+						required
+					/>
+
+					<label htmlFor="confirm-password">Confirm Password:</label>
+					<input
+						type="password"
+						id="confirm-password"
+						name="confirm-password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+						required
+					/>
+
+					<button type="submit">Create Account</button>
+				</form>
+				<p>Already have an account? <a href="#" id="sign-in-link">Sign In</a></p>
+			</div>
+		</div>
+	);
+};
+
+export default CreateAccount;
---
+++ src/components/LogInPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LogInPage = () => {
+  return (
+    <div className="container">
+      <h1>ChatGPT Crawler Site</h1>
+      <div id="signin-form" className="visible">
+        <h2>Sign In</h2>
+        {/* Add your sign-in form here */}
+      </div>
+    </div>
+  );
+};
+
+export default LogInPage;
